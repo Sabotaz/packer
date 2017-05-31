9f29bdaf6123a72dbe6272148d62dc795a93d606
Sablier <sablier@zendikar.fr>
2017-05-31 16:46:09 +0200

    refactor watcher

--- watcher.py
+++ watcher.py
@@ -8,37 +8,74 @@ from watchdog.events import FileSystemEventHandler
 import os
 import os.path
 import threading
+#import re
+#for branch reset feature:
+import regex as re
 
 lock = threading.Lock()
 
-def comp(wd, f, imports):
-    print "compile", f
-    lines = []
-    with open(wd + "/" + f, 'r') as infile:
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+        
+class ReloadSystemLib(Exception):
+    pass
+    
+class ImportLib(metaclass=Singleton):
+    wd = "."
+    def __init__(self):
+        self.libs = []
+        self.loaded_libs = []
+        
+    def load(self, lib):
+        if lib not in self.loaded_libs:
+            if lib in self.libs:
+                raise ReloadSystemLib()
+            self.libs.append(lib)
+            yield from Source(lib).process()
+            self.loaded_libs.append(lib)
+            
+    def clear(self):
+        ImportLib.__init__(self)
+        
+class Source:
+    def __init__(self, name):
+        self.requierments = []
+        self.name = name
+        
+    def process(self):
+        with open(ImportLib.wd + "/" + self.name + ".py", 'r') as infile:
+            yield from self.process_file(infile)
+            
+    def process_file(self, infile):
         for line in infile:
-            if line.startswith("import ") or (line.startswith("from ") and line.rstrip().endswith(" import *")):
-                cible = line[7:].rstrip() if line.startswith("import ") else line[5:-9].rstrip()
-                if cible in imports:
-                    continue
-                imports.add(cible)
-                if os.path.isfile(wd + "/" + cible + ".py"):
-                    lines.extend(comp(wd, cible + ".py", imports))
-                else:
-                    lines.append(line)
+            import_regexp = re.compile(r"(?|import (\w+)|from (\w+) import \*)")
+            import_statement = re.search(import_regexp, line)
+            if import_statement:
+                cible = import_statement.group(1)
+                try:
+                    yield from ImportLib().load(cible)
+                    self.requierments.append(cible)
+                except IOError:
+                    yield line
+                except ReloadSystemLib:
+                    pass
             else:
-                for fichier in imports:
-                    if fichier + "." in line:
-                        if os.path.isfile(wd + "/" + fichier + ".py"):
-                            line = line.replace(fichier + ".", "")
-                            break
-                lines.append(line)
-    return lines
+                for lib in self.requierments:
+                    if lib + "." in line:
+                        line = line.replace(lib + ".", "")
+                yield line
 
 def recompile(wd):
     lock.acquire()
-    main = comp(wd, "main.py", set())
+    ImportLib().clear()
+    ImportLib.wd = wd
+    
     with open('exange_file', 'w') as outfile:
-        for line in main:
+        for line in ImportLib().load("main"):
             outfile.write(line)
     lock.release()
 
@@ -49,20 +86,20 @@ class Handler(FileSystemEventHandler):
         self.directory = cible
 
     def on_deleted(self, event):
-        print event
+        print(event)
         recompile(self.directory)
 
     def on_modified(self, event):
         if event.event_type != "DirModifiedEvent":
-            print event
+            print(event)
             recompile(self.directory)
 
     def on_moved(self, event):
-        print event
+        print(event)
         recompile(self.directory)
 
 if __name__ == "__main__":
-    directory = raw_input()
+    directory = input()
     event_handler = Handler(directory)
     observer = Observer()
     observer.schedule(event_handler, directory, recursive=True)
